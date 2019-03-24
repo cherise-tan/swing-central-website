d020dccbae57b9c6a2f8a41e9bc70cdd912d294d
Cherise <cherisetan@live.com>
2019-03-24 20:07:11 +1300

    Add navbar scroll functionality

---
+++ public/js/index.js
@@ -0,0 +1,30 @@
+//jshint esversion:6
+
+$(document).ready(function() {
+  var startingPos = window.pageYOffset;
+
+
+  var navbarHeight = $("#navigation-bar").outerHeight();
+
+
+  $(window).scroll(function() {
+    var currentPos = window.pageYOffset;
+
+    if (startingPos < currentPos && currentPos > navbarHeight) {
+
+      $("#navigation-bar").css({
+          top: "-144px"
+      });
+
+    }
+    else {
+      $("#navigation-bar").css({
+        top: "0px"
+      });
+    }
+
+    startingPos = currentPos;
+
+  });
+
+});
