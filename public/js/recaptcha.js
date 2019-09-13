9fcd9a4c58a8c9a023decdf6126e7eb84427ae7c
Cherise <cherisetan@live.com>
2019-09-13 22:31:13 +1200

    Stop user interaction with contact form submit button until recaptcha has been checked

--- public/js/recaptcha.js
+++ public/js/recaptcha.js
@@ -1,5 +1,7 @@
 document.getElementById("formBtn").disabled = true;
+document.getElementById("formBtn").setAttribute("recaptcha-false");
 
 function enableBtn() {
     document.getElementById("formBtn").disabled = false;
+    document.getElementById("formBtn").setAttribute("recaptcha-true");
 }
